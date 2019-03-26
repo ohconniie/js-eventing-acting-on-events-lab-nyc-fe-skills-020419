389efbfd61efce2d09eed7b6fda24a08090a770b
Connie Chan <conniechan.10@gmail.com>
2019-03-26 00:25:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ function onKeyDown(event) {
   }
 }
 
+function moveDodgerRight() {
+  const currentPosition = parseInt(dodger.style.left)
+  dodger.style.left = currentPosition + 5 +'px'
+}
+
 document.addEventListener('keydown', onKeyDown)
 
