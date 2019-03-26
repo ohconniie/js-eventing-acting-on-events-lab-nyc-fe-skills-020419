e359ff8e7972f49727d6cae91fd0c80c278129fc
Connie Chan <conniechan.10@gmail.com>
2019-03-26 00:17:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const dodger = document.getElementById('dodger')
 function onKeyDown(event) {
   if (event.keyCode === 37) {
     // how do we actually move this left  
-    console.log(dodger.style.left)
+    const. currentPosition = parseInt(dodger.style.left)
+    console.log(dodger.style.left = currentPosition - 5 +'px')
   }
 }
 
