8bfb00b5a7ec30cc4bcefaf5093dcf113ffcae58
Connie Chan <conniechan.10@gmail.com>
2019-03-26 00:13:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+// 1 - find the element
 const dodger = document.getElementById('dodger')
 
-function onKeyDown (event) {
-  console.log(event)
+function onKeyDown(event) {
   if (event.keyCode === 37) {
+    // how do we actually move this left  
     console.log(dodger.style.left)
   }
 }
 
+// 2 - Add the event listener
 document.addEventListener('keydown', onKeyDown)
